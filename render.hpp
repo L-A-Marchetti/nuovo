5551fdb907434267aced01474cd0e68de1de6387
lmarchet <lucasmarchetti21@gmail.com>
2024-12-13 17:24:36 +0100

    color & sets

--- render.hpp
+++ render.hpp
@@ -7,12 +7,12 @@
 #define SCREEN "fonts/7segment.ttf"
 #define BLUE_GRAY (SDL_Color){46, 67, 71, 255}
 #define GRAY (SDL_Color){196, 200, 194, 255}
-#define DARK_ORANGE (SDL_Color){239, 125, 58, 255}
+#define DARK_ORANGE (SDL_Color){106, 186, 230, 255}
 #define DARK_GRAY (SDL_Color){90, 99, 107, 255}
 #define B_LIGHT (SDL_Color){237, 226, 195, 255}
 #define B_SHADOW (SDL_Color){184, 174, 145, 255}
-#define LED (SDL_Color){252, 0, 0, 255}
-#define SCREEN_BG (SDL_Color){128, 0, 0, 255}
+#define LED (SDL_Color){127, 255, 255, 255} // 127, 255, 255
+#define SCREEN_BG (SDL_Color){2, 189, 189, 255} // 3, 255, 255
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
